15752543501676f6b369edb599cda1c286ed408e
Tom McLaughlin <tom@serverlessops.io>
2025-01-03 16:11:34 -0500

    switch from dataclasses to TypedDict
    Going to see how I like this approach. maybe switch to it going forward. This was prompted by finding out my dataclass wasn't working as expected due to inhertiting a TypedDict

--- src/common/common/model/account.py
+++ src/common/common/model/account.py
@@ -1,12 +1,9 @@
-from dataclasses import dataclass
 from typing import List
 
 from mypy_boto3_organizations.type_defs import AccountTypeDef
 
-@dataclass
 class AccountType(AccountTypeDef):
     pass
 
-@dataclass
 class AccountTypeWithTags(AccountType):
     Tags: List[dict]
