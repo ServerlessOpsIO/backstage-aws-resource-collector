e358da26c77a2294427a7dce82ef5ec39ec470d4
Tom McLaughlin <tom@serverlessops.io>
2025-05-28 12:15:57 -0400

    Add RemoveEntityFromCatalog function

---
+++ src/handlers/RemoveEntityFromCatalog/function.py
@@ -0,0 +1,73 @@
+
+'''Remove Entity from catalog'''
+import os
+import json
+import requests
+
+from aws_lambda_powertools.logging import Logger
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.utilities.data_classes import (
+    event_source,
+    SQSEvent
+)
+
+from common.model.entity import Entity
+from common.util.jwt import JwtAuth
+
+LOGGER = Logger(utc=True)
+
+CATALOG_ENDPOINT = os.environ.get('CATALOG_ENDPOINT', 'MUST_SET_CATALOG_ENDPOINT')
+
+CLIENT_ID = os.environ.get('CLIENT_ID', 'MUST_SET_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET', 'MUST_SET_CLIENT_SECRET')
+JWT = JwtAuth(CLIENT_ID, CLIENT_SECRET)
+
+class RemoveEntityFromCatalogError(Exception):
+    '''Remove Entity From Catalog Error'''
+    def __init__(self, entity: Entity) -> None:
+        name = entity['metadata']['title']
+        super().__init__('Failed to remove entity from catalog: {}'.format(name))
+
+def _remove_entity_from_catalog(entity: Entity, auth: JwtAuth) -> requests.Response:
+    '''Add entity to catalog'''
+    r = requests.delete(
+        '/'.join([
+            CATALOG_ENDPOINT,
+            entity['metadata']['namespace'],
+            entity['kind'].lower(),
+            entity['metadata']['name']
+        ]),
+        headers={
+            'Content-Type': 'application/json'
+        },
+        auth=auth,
+        timeout=10
+    )
+
+    if not r.ok:
+        LOGGER.error('Failed to remove entity from catalog', extra={'response': r.text, 'entity': entity})
+        raise RemoveEntityFromCatalogError(entity)
+
+    return r
+
+
+def _main(entity: Entity) -> requests.Response:
+    '''Publish account to catalog.'''
+    return _remove_entity_from_catalog(entity, JWT)
+
+
+@LOGGER.inject_lambda_context
+@event_source(data_class=SQSEvent)
+def handler(event: SQSEvent, _: LambdaContext) -> None:
+    '''Event handler'''
+    LOGGER.debug('Event', extra={"message_object": event._data})
+    for record in event.records:
+        body = json.loads(record.body)
+        if 'responsePayload' in body:
+            # Handle the case where message was sent to SQS via Lambda Destination
+            entity = Entity(**body['responsePayload'])
+        else:
+            entity = Entity(**body)
+        _main(entity)
+
+    return
---
+++ tests/unit/handlers/RemoveEntityFromCatalog/test_function.py
@@ -0,0 +1,241 @@
+'''Test RemoveEntityFromCatalog'''
+
+# pylint: disable=missing-function-docstring,missing-module-docstring,unused-argument,redefined-outer-name,protected-access
+
+import json
+import os
+from time import time
+from types import ModuleType
+from typing import Generator
+
+import jsonschema
+
+import pytest
+from pytest_mock import MockerFixture
+import requests_mock
+
+from aws_lambda_powertools.utilities.data_classes import SQSEvent
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+from common.model.entity import Entity
+from common.test.aws import create_lambda_function_context
+from common.util.jwt import AUTH_ENDPOINT, JwtAuth
+
+FN_NAME = 'RemoveEntityFromCatalog'
+DATA_DIR = './data'
+FUNC_DATA_DIR = os.path.join(DATA_DIR, 'handlers', FN_NAME)
+EVENT = os.path.join(FUNC_DATA_DIR, 'event.json')
+EVENT_SCHEMA = os.path.join(FUNC_DATA_DIR, 'event.schema.json')
+EVENT_DATA = os.path.join(FUNC_DATA_DIR, 'event-data.json')
+EVENT_DATA_SCHEMA = os.path.join(FUNC_DATA_DIR, 'event-data.schema.json')
+
+### Fixtures
+
+# FIXME: Need to handle differences between powertools event classes and the Event class
+# Event
+@pytest.fixture()
+def mock_event_data(e=EVENT_DATA) -> Entity:
+    '''Return event data object'''
+    with open(e) as f:
+        return Entity(**json.load(f))
+
+@pytest.fixture()
+def event_data_schema(schema=EVENT_DATA_SCHEMA):
+    '''Return an data schema'''
+    with open(schema) as f:
+        return json.load(f)
+
+@pytest.fixture()
+def mock_event(e=EVENT) -> SQSEvent:
+    '''Return a function event'''
+    with open(e) as f:
+        return SQSEvent(json.load(f))
+
+@pytest.fixture()
+def event_schema(schema=EVENT_SCHEMA):
+    '''Return an event schema'''
+    with open(schema) as f:
+        return json.load(f)
+
+@pytest.fixture()
+def requests_mocker() -> requests_mock.Mocker:
+    '''Return a requests mock'''
+    # NOTE: Use as a decerator with Python 3 appears broken so use fixture.
+    # ref. https://github.com/pytest-dev/pytest/issues/2749
+    return requests_mock.Mocker()
+
+@pytest.fixture()
+def mock_endpoint() -> str:
+    '''Return a mock endpoint'''
+    return 'https://api.example.com/catalog'
+
+@pytest.fixture()
+def mock_auth(
+    mocker: MockerFixture,
+    requests_mocker: requests_mock.Mocker,
+) -> Generator[JwtAuth, None, None]:
+    '''Yield a JWT Auth object'''
+    requests_mocker.register_uri(
+        requests_mock.POST,
+        AUTH_ENDPOINT,
+        status_code=200,
+        json={'access_token': 'token'}
+    )
+
+    jwt = JwtAuth('clientId', 'clientSecret')
+    mocker.patch.object(jwt, 'token', 'jwt-token')
+    mocker.patch.object(jwt, 'expiration', int(time()) + 600)
+
+    yield jwt
+
+
+# Function
+@pytest.fixture()
+def mock_context(function_name=FN_NAME):
+    '''context object'''
+    return create_lambda_function_context(function_name)
+
+@pytest.fixture()
+def mock_fn(
+    mock_endpoint: str,
+    mock_auth: JwtAuth,
+    requests_mocker: requests_mock.Mocker,
+    mocker: MockerFixture
+) -> Generator[ModuleType, None, None]:
+    '''Return mocked function'''
+    import src.handlers.RemoveEntityFromCatalog.function as fn
+
+    # NOTE: use mocker to mock any top-level variables outside of the handler function.
+    mocker.patch(
+        'src.handlers.RemoveEntityFromCatalog.function.JWT',
+        mock_auth
+    )
+
+    mocker.patch(
+        'src.handlers.RemoveEntityFromCatalog.function.CATALOG_ENDPOINT',
+        mock_endpoint
+    )
+
+    # We can also use requests_mocker within tests too if necxessary
+    with requests_mocker:
+        requests_mocker.register_uri(
+            requests_mock.ANY,
+            requests_mock.ANY,
+            status_code=200,
+        )
+        yield fn
+
+
+### Data validation tests
+def test_validate_data(mock_event_data, event_data_schema):
+    '''Test event against schema'''
+    jsonschema.Draft7Validator(mock_event_data, event_data_schema)
+
+def test_validate_event(mock_event, event_schema):
+    '''Test event against schema'''
+    jsonschema.Draft7Validator(mock_event._data, event_schema)
+
+
+### Code Tests
+def test_RemoveEntityFromCatalogError(
+    mock_event_data: Entity,
+    mock_fn: ModuleType
+    ):
+    '''Test RemoveEntityFromCatalogError class'''
+    e = mock_fn.RemoveEntityFromCatalogError(mock_event_data)
+    assert str(e) == 'Failed to remove entity from catalog: MockResource'
+
+def test__remove_entity_from_catalog(
+    mock_fn: ModuleType,
+    mock_event_data: Entity,
+    mock_endpoint: str,
+    mock_auth: JwtAuth,
+    requests_mocker: requests_mock.Mocker,
+):
+    r = mock_fn._remove_entity_from_catalog(mock_event_data, mock_auth)
+
+    assert requests_mocker.called is True
+    assert requests_mocker.call_count == 1
+    assert r.ok is True
+    assert r.request.method == 'DELETE'
+    assert r.request.url == '{}/{}/{}/{}'.format(
+        mock_endpoint,
+        mock_event_data['metadata']['namespace'],
+        mock_event_data['kind'].lower(),
+        mock_event_data['metadata']['name']
+    )
+
+
+def test__remove_entity_from_catalog_fails(
+    mock_fn: ModuleType,
+    mock_event_data: Entity,
+    mock_auth: JwtAuth,
+    requests_mocker: requests_mock.Mocker,
+):
+    '''Test _add_account_to_catalog function'''
+    requests_mocker.register_uri(
+        requests_mock.DELETE,
+        requests_mock.ANY,
+        status_code=403,
+    )
+
+    with pytest.raises(mock_fn.RemoveEntityFromCatalogError):
+        mock_fn._remove_entity_from_catalog(mock_event_data, mock_auth)
+
+
+def test__main(
+    mock_fn: ModuleType,
+    mock_event_data: Entity,
+    mocker: MockerFixture
+):
+    '''Test _main function'''
+    mocker.patch.object(
+        mock_fn,
+        'JwtAuth',
+        client_id='clientId',
+        client_secret='clientSecret',
+        token='token'
+    )
+
+    mock_fn._main(mock_event_data)
+
+
+def test_handler_with_sqs_event(
+    mock_fn: ModuleType,
+    mock_context: LambdaContext,
+    mock_event_data: Entity,
+    mock_event: SQSEvent,
+    mocker: MockerFixture
+):
+    '''Test calling handler'''
+    # Call the function
+    mocker.patch.object(
+        mock_fn,
+        'JwtAuth',
+        client_id='clientId',
+        client_secret='clientSecret',
+        token='token'
+    )
+
+    mock_event._data['Records'][0]['body'] = json.dumps(mock_event_data)
+    mock_fn.handler(mock_event, mock_context)
+
+def test_handler_with_lambda_destinations_sqs_event(
+    mock_fn: ModuleType,
+    mock_context: LambdaContext,
+    mock_event_data: Entity,
+    mock_event: SQSEvent,
+    mocker: MockerFixture
+):
+    '''Test calling handler'''
+    # Call the function
+    mocker.patch.object(
+        mock_fn,
+        'JwtAuth',
+        client_id='clientId',
+        client_secret='clientSecret',
+        token='token'
+    )
+
+    mock_event._data['Records'][0]['body'] = json.dumps({ "responsePayload": mock_event_data })
+    mock_fn.handler(mock_event, mock_context)
